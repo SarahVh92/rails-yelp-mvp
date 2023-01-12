5b905d60edd0472950509da710cc81ef50d5d21b
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-01-12 21:11:14 +0900

    Add destroy review action and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: %i[index create new show] do
+    # the new and create methods don't use :id or any unique paths, and so should be nested
     resources :reviews, only: %i[new create]
   end
+  # the destroy method uses :id, so doesn't need to be nested
+  resources :reviews, only: [:destroy]
 end
